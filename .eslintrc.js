197fc7a7a01f33063eb1a1d2e9501f70a0800de8
陈书航 <chen.shuhang@iwhalecloud.com>
2023-01-10 15:59:10 +0800

    test githooks 7

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,8 @@ module.exports = {
   root: true,
   extends: [require.resolve("@umijs/fabric/dist/eslint")],
   rules: {
-    "import/no-extraneous-dependencies": 0,
-    "jsx-a11y/alt-text": 0,
     "no-unused-expressions": 0,
     "@typescript-eslint/no-unused-vars": 0,
-    "react-hooks/exhaustive-deps": 0,
   },
   globals: {},
 };
